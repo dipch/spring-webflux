979c9122c804443b77715f9e95d73cd1338444c5
Dip Chowdhury <dipchowdhury2@gmail.com>
2022-09-25 13:35:34 +0600

    repository and controler setup

---
+++ src/main/java/com/example/springwebflux/controller/AnimeController.java
@@ -0,0 +1,25 @@
+package com.example.springwebflux.controller;
+
+import com.example.springwebflux.domain.Anime;
+import com.example.springwebflux.repository.AnimeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("animes")
+@Slf4j
+@RequiredArgsConstructor
+public class AnimeController {
+
+    private final AnimeRepository animeRepository;
+
+    @GetMapping
+    public Flux<Anime> listAll(){
+        return animeRepository.findAll();
+    }
+
+}
