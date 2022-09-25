31457f694f876d54eeb52a2196916b40476ab2e6
Dip Chowdhury <dipchowdhury2@gmail.com>
2022-09-25 13:16:58 +0600

    initial commit

---
+++ schema.sql
@@ -0,0 +1,12 @@
+create table anime.anime
+(
+    id serial not null,
+    name varchar not null
+);
+
+create unique index anime_id_uindex
+    on anime.anime (id);
+
+alter table anime.anime
+    add constraint anime_pk
+        primary key (id);
